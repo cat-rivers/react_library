201f76b028453ad36476125889ebd2ae6bb8322f
Cat-Rivers <catalina.riveros.m@gmail.com>
2023-01-23 13:42:21 +0200

    add get and put request services for books db

---
+++ awesome_library/src/services/servicesBooks.js
@@ -0,0 +1,12 @@
+import axios from "axios";
+const baseUrl = "http://localhost:3001/books";
+
+const getAllBooks = () => {
+  return axios.get(baseUrl).then(repsonse => Response.data);
+};
+
+const editBook = (changedbook, id) => {
+  return axios.put(`${baseUrl}/${id}`, changedbook);
+};
+
+export { getAllBooks, editBook };
