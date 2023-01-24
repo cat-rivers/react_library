ec719528ade6a8f3a286e39fdc37e7e08b3d0b2f
Cat Rivers <cat.rivers.blog@gmail.com>
2023-01-24 12:14:49 +0200

    uncommented stuff

--- awesome_library/src/App.js
+++ awesome_library/src/App.js
@@ -1,33 +1,35 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-// import Home from "./components/Home";
-// import Search from "./components/Search";
-// import MyPage from "./components/MyPage";
-// import Login from "./components/Login";
-// import Signup from "./components/Signup";
-// import BookById from "./components/BookById";
-// import SignupSuccessful from "./components/SignupSuccessful";
-// import Layout from "./components/Layout";
+import Home from "./components/Home";
+import Search from "./components/Search";
+import MyPage from "./components/MyPage";
+import Login from "./components/Login";
+import Signup from "./components/Signup";
+import BookById from "./components/BookById";
+import SignupSuccessful from "./components/SignupSuccessful";
+import Layout from "./components/Layout";
 import BookCardContainer from "./components/BookContainer";
 
 function App() {
   return (
-    <BookCardContainer />
-    // <Router>
-    //   <Routes>
-    //     <Route path="/" element={<Layout />}>
-    //       <Route index element={<Home />} />
-    //       <Route path="search" element={<Search />} />
-    //       <Route path="mypage" element={<MyPage />} />
-    //       <Route
-    //         path="signup/signupsuccessful"
-    //         element={<SignupSuccessful />}
-    //       />
-    //       <Route path="login" element={<Login />} />
-    //       <Route path="signup" element={<Signup />} />
-    //       <Route path="search/book/{id}" element={<BookById />} />
-    //     </Route>
-    //   </Routes>
-    // </Router>
+    <>
+      <BookCardContainer />
+      <Router>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<Home />} />
+            <Route path="search" element={<Search />} />
+            <Route path="mypage" element={<MyPage />} />
+            <Route
+              path="signup/signupsuccessful"
+              element={<SignupSuccessful />}
+            />
+            <Route path="login" element={<Login />} />
+            <Route path="signup" element={<Signup />} />
+            <Route path="search/book/{id}" element={<BookById />} />
+          </Route>
+        </Routes>
+      </Router>
+    </>
   );
 }
 
