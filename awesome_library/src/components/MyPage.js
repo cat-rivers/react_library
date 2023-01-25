3efdd2c7086be5923951f5469a39cda94667b3fd
tahl74 <tahlholm@gmail.com>
2023-01-25 14:14:13 +0200

    MyPage-Mockup & package.locks

--- awesome_library/src/components/MyPage.js
+++ awesome_library/src/components/MyPage.js
@@ -1,6 +1,24 @@
+import "./MyPage.css";
 const MyPage = () => {
     return (
-        <h3>Placeholder</h3>
+            <div><br /><br /><br /><br />
+                <fieldset>
+                    <legend>Hi Teppo Testaaja, welcome to library </legend>
+                            <div>You have borrowed these books:<br />
+                            <ul>
+
+                            <i>Eric Elliott</i><br />
+                            Programming JavaScript Applications
+                            Robust Web architecture with Node, HTML5, and Modern JS Libraries
+                            <br />
+                            2014</ul>
+                            Borrowed until: 12.12.2020
+                            </div>                            <br />
+                        
+                            <ul><button>Return</button></ul><br />
+                        <label>State: Signed in.</label>
+                </fieldset>
+            </div>
     )
 }
 
