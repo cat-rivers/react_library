6b5017db1a3cac8e02b99f60039c9a27cb7e9e7c
Cat-Rivers <catalina.riveros.m@gmail.com>
2023-01-25 15:10:27 +0200

    add usestate and useEffect to App.js

--- awesome_library/src/App.js
+++ awesome_library/src/App.js
@@ -1,21 +1,22 @@
-import { BrowserRouter as Router, Routes, Route} from "react-router-dom"
-import Home from "./components/Home"
-import Search from "./components/Search"
-import MyPage from "./components/MyPage"
-import Login from "./components/Login"
-import Signup from "./components/Signup"
-import BookById from "./components/BookById"
-import SignupSuccessful from "./components/SignupSuccessful"
-import Header from "./components/Header"
-import Footer from "./components/Footer"
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./components/Home";
+import Search from "./components/Search";
+import MyPage from "./components/MyPage";
+import Login from "./components/Login";
+import Signup from "./components/Signup";
+import BookById from "./components/BookById";
+import SignupSuccessful from "./components/SignupSuccessful";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 import { getAllBooks } from "./services/servicesBooks";
+import { useState, useEffect } from "react";
 
 function App() {
   const [bookDetails, setBookDetails] = useState([]);
 
   useEffect(() => {
     console.log("fetching data!");
-    getAllBooks().then((books) => {
+    getAllBooks().then(books => {
       console.log(books);
       setBookDetails(books);
     });
@@ -23,17 +24,17 @@ function App() {
 
   return (
     <Router>
-      <Header/>
+      <Header />
       <Routes>
-          <Route index element={<Home />} />
-          <Route path="search" element={<Search bookDetails={bookDetails} />} />
-          <Route path="mypage" element={<MyPage/>} />
-          <Route path="signup/signupsuccessful" element={<SignupSuccessful/>} />
-          <Route path="login" element={<Login/>} />
-          <Route path="signup" element={<Signup/>} />
-          <Route path="search/book/{id}" element={<BookById/>} />
+        <Route index element={<Home />} />
+        <Route path="search" element={<Search bookDetails={bookDetails} />} />
+        <Route path="mypage" element={<MyPage />} />
+        <Route path="signup/signupsuccessful" element={<SignupSuccessful />} />
+        <Route path="login" element={<Login />} />
+        <Route path="signup" element={<Signup />} />
+        <Route path="search/book/{id}" element={<BookById />} />
       </Routes>
-      <Footer/>
+      <Footer />
     </Router>
   );
 }
