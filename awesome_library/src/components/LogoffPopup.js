b7a2ac58086293d55b3dc12ce4ca51984045aec0
tahl74 <tahlholm@gmail.com>
2023-01-26 15:13:20 +0200

    eka versio

--- awesome_library/src/components/LogoffPopup.js
+++ awesome_library/src/components/LogoffPopup.js
@@ -1,7 +1,38 @@
-const LogoffPopup = () => {
-    return (
-        <div style={{cursor: 'pointer'}}>Log Off</div>
-    )
-}
+import Modal from "react-modal";
+import { useState } from "react";
+import "./LoginPopup.css";
 
+const LogoffPopup = () => {
+    const [modalIsOpen, setModalIsOpen] = useState(false);
+	Modal.setAppElement('#root');
+	const closeModal = () => setModalIsOpen(false)
+
+    const customStyles = {
+		content: {
+			inset: '30% auto auto 50%', 
+			transform: 'translate(-50%, -50%)',
+			textAlign: "center",
+		},
+	};
+
+
+    return (<>
+		<div className="navbarlink" onClick={() => setModalIsOpen(true)}>Logoff</div>
+		<Modal
+			isOpen={modalIsOpen}
+			onRequestClose={closeModal}
+			contentLabel="Login"
+            style={customStyles}
+            >
+	    
+			<h3>Logoff</h3>
+
+            <form>
+                YYZ
+            </form>
+            <br/>
+			<button onClick={closeModal}>Submit</button>
+		</Modal>
+	</>)
+}
 export default LogoffPopup;
