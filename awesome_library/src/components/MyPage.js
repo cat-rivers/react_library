ecafdc145e83632da2fedf081006bbfe7ae23994
ewoswan <ewoswan@yahoo.com>
2023-01-26 12:16:49 +0200

    modify header.css and MyPage.js

--- awesome_library/src/components/MyPage.js
+++ awesome_library/src/components/MyPage.js
@@ -1,4 +1,25 @@
-const MyPage = () => {
+import { useState } from "react";
+
+const MyPage = ({ bookDetails }) => {
+    const [login, setLogin] = useState("")
+    const [loginApproved, setLoginApproved] = useState(false);
+
+    const filterBooks = bookDetails.filter((book) => {
+        return (
+          book.title.toLowerCase().includes(login.toLowerCase()) ||
+          book.author.toLowerCase().includes(login.toLowerCase()) ||
+          book.isbn.includes(login)
+        );
+    });
+
+    const myPage = () => {
+        // combine filterBooks and Teemu my page mockup
+    } 
+
+    const message = <p> Login is wrong, please type again </p>;
+
+    const displayedBook = loginApproved ? {myPage} : {message}
+
     return (
         <h3>Placeholder</h3>
     )
