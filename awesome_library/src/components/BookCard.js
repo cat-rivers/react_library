8806a73aba71eaf9f2ba21f6cc510a9e8140b3fc
Cat Rivers <cat.rivers.blog@gmail.com>
2023-01-26 12:39:59 +0200

    add small version and detailed version of card

--- awesome_library/src/components/BookCard.js
+++ awesome_library/src/components/BookCard.js
@@ -1,21 +1,42 @@
 import BookCopyCard from "./BookCopyCard";
 import "./BookCard.css";
 
-const BookCard = ({ book }) => (
-  <div className="bookCard">
-    <div className="bookInfo">
-      <div className="header">
-        <p>{book.author}</p>
-        <h2>{book.title}</h2>
-        <p>{book.published.substr(0, 4)}</p>
+const BookCard = ({ book }) => {
+  const isModalOn = false;
+  const bigBookCard = () => {
+    return (
+      <div className="bookCard">
+        <div className="bookInfo">
+          <div className="header">
+            <p>{book.author}</p>
+            <h2>{book.title}</h2>
+            <p>{book.published.substr(0, 4)}</p>
+          </div>
+          <p>{book.description}</p>
+        </div>
+        <div className="bookStatus">
+          <h3>Copies in Library</h3>
+          <BookCopyCard copies={book.copies} />
+        </div>
       </div>
-      <p>{book.description}</p>
-    </div>
-    <div className="bookStatus">
-      <h3>Copies in Library</h3>
-      <BookCopyCard copies={book.copies} />
-    </div>
-  </div>
-);
+    );
+  };
+
+  const bookPreviewCard = () => {
+    return (
+      <div className="bookCard">
+        <div className="bookInfo">
+          <div className="header">
+            <p>{book.author}</p>
+            <h2>{book.title}</h2>
+          </div>
+          <p>{book.description.substring(0, 200)}...</p>
+        </div>
+        <button className="modalBookBtn"> More..</button>
+      </div>
+    );
+  };
+  return isModalOn ? bigBookCard() : bookPreviewCard();
+};
 
 export default BookCard;
