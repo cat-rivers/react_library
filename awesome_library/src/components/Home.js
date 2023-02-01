8c22db376c41a894ca5963e4ddf56ad9f5bb475f
tahl74 <tahlholm@gmail.com>
2023-02-01 14:53:43 +0200

    Modified and fine tuned layout, in progress..

--- awesome_library/src/components/Home.js
+++ awesome_library/src/components/Home.js
@@ -10,8 +10,8 @@ const Home = () => {
                 <p><b>About us:</b></p>
                 <p>Log in with your library card information and you can e.g. borrow books and get access 
                     to your own electronic bookshelf.</p>
-                <p>If you cannot log in with the information you entered, please contact your library staff.
-                </p>
+                <p>If you cannot log in with the information you entered, please contact your library staff.</p>
+                <p>***</p>
                 <p>Yours,<br/>
                     your Awesome Library team Red</p>
             </div>
