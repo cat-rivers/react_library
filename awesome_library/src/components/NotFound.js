052a10457fac2a14a79ba489ba6c752c5f57fdb8
Hannu Herraniemi <hannu.herraniemi@gmail.com>
2023-02-02 13:38:41 +0200

    rewrote 404 txt

--- awesome_library/src/components/NotFound.js
+++ awesome_library/src/components/NotFound.js
@@ -1,8 +1,11 @@
 const NotFound = () => {
     return (
         <div>
-           <h1>Oops! You seem to be lost.</h1>
-            <p>How about a game of Snake while you're here.</p><br/>
+           <h1>Oops! We got ourselves a 404 over here!</h1>
+            <p>
+                How about a game of Snake while you're at it..?<br/>
+                Click on the game area. Use arrow keys to move the snake.
+            </p><br/>
            <center>
             <iframe 
                 width="580"
